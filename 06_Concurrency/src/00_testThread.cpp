fe895954d4111d5eb1178a4da6f069e94c98857b
Simon Nguyen <simon.nguyen@ssw-trading.com>
2022-05-16 07:43:25 +0200

    44. Concurrency: first lesson tests

---
+++ 06_Concurrency/src/00_testThread.cpp
@@ -0,0 +1,26 @@
+#include <thread>
+#include <vector>
+#include <algorithm>
+
+int main2()
+
+{
+    /*
+    std::vector<std::thread> threads;
+    int nThreads = 4;
+    for (int i= 0; i < nThreads; ++i)
+    {
+        threads.emplace_back(std::thread([]() {
+            while (true);
+        }));
+
+    }
+
+    // wait for threads to finish before leaving main
+    std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+        t.join();
+    });
+
+*/
+    return 0;
+}
