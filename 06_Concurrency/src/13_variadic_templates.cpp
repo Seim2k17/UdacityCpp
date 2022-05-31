aba33470b999970e26f35b64d349ba00e4452aea
Simon Nguyen <sng198022@gmail.com>
2022-05-31 08:58:59 +0200

    46. Concurrency: progress until 1.5

---
+++ 06_Concurrency/src/13_variadic_templates.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+void printName(std::string name, int waitTime)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(waitTime));
+    std::cout << "Name (from Thread) = " << name << std::endl;
+}
+
+int main()
+{
+    std::string name1 = "MyThread1";
+    std::string name2 = "MyThread2";
+
+    // starting threads using value-copy and move semantics 
+    std::thread t1(printName, name1, 50);
+    std::thread t2(printName, std::move(name2), 100);
+
+    // wait for threads before returning
+    t1.join();
+    t2.join();
+
+    // print name from main
+    // name1 available after join
+    std::cout << "Name (from Main) = " << name1 << std::endl;
+    // name2 ist not available after join, because it was moved
+    std::cout << "Name (from Main) = " << name2 << std::endl;
+
+    return 0;
+}
