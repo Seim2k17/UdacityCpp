32b867eec649738a177dce292c19e5563e1cfd1d
Simon Nguyen <simon.nguyen@ssw-trading.com>
2022-07-08 08:31:54 +0200

    60. Concurrency: Init L3
    - add L1 official solution

---
+++ 06_Concurrency/src/42_mutex_lockguard2_deadlock.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+ 
+std::mutex mutex1, mutex2;
+ 
+void ThreadA()
+{
+    // Creates deadlock problem
+    std::lock_guard<std::mutex> lock2(mutex2);
+    // do something to make the deadlock visible to fast machines
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+    std::cout << "Thread A" << std::endl;
+    std::lock_guard<std::mutex> lock1(mutex1);
+    
+}
+ 
+void ThreadB()
+{
+    // Creates deadlock problem
+    std::lock_guard<std::mutex> lock1(mutex1);
+    std::cout << "Thread B" << std::endl;
+    std::lock_guard<std::mutex> lock2(mutex2);
+}
+ 
+void ExecuteThreads()
+{
+    std::thread t1( ThreadA );
+    std::thread t2( ThreadB );
+ 
+    t1.join();
+    t2.join();
+ 
+    std::cout << "Finished" << std::endl;
+}
+ 
+int main()
+{
+    ExecuteThreads();
+ 
+    return 0;
+}
---
+++ 06_Concurrency/src/43_mutex_lockguard3_nodeadlock.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+ 
+std::mutex mutex1, mutex2;
+ 
+void ThreadA()
+{
+    // Ensure that locks are always executed in the same order
+    std::lock(mutex1, mutex2);
+    std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
+    // do something to make the deadlock visible to fast machines
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+    std::cout << "Thread A" << std::endl;
+    std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
+    
+}
+ 
+void ThreadB()
+{
+    std::lock(mutex1, mutex2);
+    std::lock_guard<std::mutex> lock1(mutex1, std::adopt_lock);
+    // do something to make the deadlock visible to fast machines
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+    std::cout << "Thread B" << std::endl;
+    std::lock_guard<std::mutex> lock2(mutex2, std::adopt_lock);
+}
+ 
+void ExecuteThreads()
+{
+    std::thread t1( ThreadA );
+    std::thread t2( ThreadB );
+ 
+    t1.join();
+    t2.join();
+ 
+    std::cout << "Finished" << std::endl;
+}
+ 
+int main()
+{
+    ExecuteThreads();
+ 
+    return 0;
+}
